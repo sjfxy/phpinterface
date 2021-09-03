c8161dfcbfee5dd60c18ed111384927ef960fa30
Shijun <986244259@qq.com>
2021-09-03 14:27:51 +0800

    同步更新

--- src/Php/Grpc/Votes/PaySvcClient.php
+++ src/Php/Grpc/Votes/PaySvcClient.php
@@ -3,73 +3,17 @@
 
 namespace Votes;
 
-use Client\GrpcBaseClient;
-use Grpc\ChannelCredentials;
 /**
  */
 class PaySvcClient extends \Grpc\BaseStub {
 
-    const SERVICENAME = "vote.svc";
-
-    /**
-     * @var GrpcBaseClient
-     */
-    private $baseClient;
-
-    /**
-     * @var string
-     */
-    private $servciename;
-
-    /**
-     * @var string
-     */
-    private $hostname;
-
-    /**
-     * @param string $hostname
-     */
-    public function setHostname(string $hostname): void
-    {
-        $this->hostname = $hostname;
-    }
-
-
-
-    /**
-     * @param string $servciename
-     */
-    public function setServciename(string $servciename): void
-    {
-        $this->servciename = $servciename;
-    }
     /**
      * @param string $hostname hostname
      * @param array $opts channel options
      * @param \Grpc\Channel $channel (optional) re-use channel object
      */
-    public function __construct($host="") {
-
-
-        if (empty($this->baseClient))
-        {
-            $this->baseClient = new GrpcBaseClient();
-        }
-        if (empty($this->servciename))
-        {
-            $this->servciename = self::SERVICENAME;
-        }
-        if (empty($host))
-        {
-            $hostname = $this->baseClient->Services($this->servciename);
-        }else{
-            $hostname = $host;
-        }
-        $opts = [
-            'credentials' => ChannelCredentials::createInsecure(),
-        ];
-        //这里我们进行拦截器处理
-        parent::__construct($hostname, $opts, null);
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
     }
 
     /**
